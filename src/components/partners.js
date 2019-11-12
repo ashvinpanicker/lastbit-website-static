49433e87cf0ecc1de51ed722cc7115a6c047541d
agnjkafgh <agnjkafgh@gmail.com>
2019-11-12 13:39:49 -0800

    charlie lee 🎉

--- src/components/partners.js
+++ src/components/partners.js
@@ -7,98 +7,123 @@ import buildit from "../../static/images/Logo4.svg"
 import brinc from "../../static/images/Logo5.svg"
 import artesian from "../../static/images/Logo6.svg"
 import skydeck from "../../static/images/skydeck1.png"
+import charlie from "../../static/images/charlie.png"
 import hatch from "../../static/images/hatch.png"
 import fulgur from "../../static/images/fulgur-logo.svg"
 
 const Partners = () => (
-  <div className={"showcase"}>
-    <div style={{ textAlign: "center", marginTop: 50 }}>
-      <h2 style={{ fontSize: 32 }}>Backed By</h2>
-      <div className={"row"}>
-        <div className={"col-3"}>
-          <img
-            src={fulgur}
-            className={"partner_logo"}
-            alt={"fulgur_ventures"}
-            style={{ cursor: "pointer" }}
-            onClick={() => window.open("https://fulgur.ventures/")}
-          />
+    <div className={"showcase"}>
+        <div style={{ textAlign: "center", marginTop: 50 }}>
+            <h2 style={{ fontSize: 32 }}>Backed By</h2>
+            <div style={{ padding: "25px 0px" }}></div>
+            <div className={"row"}>
+                <div className={"col-4"}>
+                    <img
+                        src={fulgur}
+                        className={"partner_logo"}
+                        alt={"fulgur_ventures"}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => window.open("https://fulgur.ventures/")}
+                    />
+                </div>
+                <div className={"col-4"}>
+                    <img
+                        src={skydeck}
+                        className={"partner_logo"}
+                        alt={"skydeck"}
+                        style={{ cursor: "pointer", padding: "5px 0 0 0" }}
+                        onClick={() =>
+                            window.open("https://skydeck.berkeley.edu/")
+                        }
+                    />
+                </div>
+                <div className={"col-4"}>
+                    <img
+                        src={charlie}
+                        className={"partner_logo"}
+                        alt={"charlie_lee"}
+                        style={{
+                            cursor: "pointer",
+                            paddingTop: 30,
+                            maxWidth: 200,
+                        }}
+                        onClick={() =>
+                            window.open("https://twitter.com/satoshilite")
+                        }
+                    />
+                </div>
+            </div>
+            <div className={"row"}>
+                <div className={"col-4"}>
+                    <img
+                        src={buildit}
+                        className={"partner_logo"}
+                        alt={"buildit"}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => window.open("https://www.buildit.lv/")}
+                    />
+                </div>
+                <div className={"col-4"}>
+                    <img
+                        src={brinc}
+                        className={"partner_logo"}
+                        alt={"brinc"}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => window.open("https://www.brinc.io/")}
+                    />
+                </div>
+                <div className={"col-4"}>
+                    <img
+                        src={artesian}
+                        className={"partner_logo"}
+                        alt={"artesian"}
+                        // style={{ cursor: "pointer" }}
+                    />
+                </div>
+            </div>
+
+            <div className={"row"}>
+                <div className={"col-4"}>
+                    <img
+                        src={hatch}
+                        className={"partner_logo"}
+                        alt={"digital_ocean"}
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                            window.open("https://www.digitalocean.com/hatch/")
+                        }
+                    />
+                </div>
+                <div className={"col-4"}>
+                    <img
+                        src={mongodb}
+                        className={"partner_logo"}
+                        alt={"mongodb"}
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                            window.open(
+                                "https://www.mongodb.com/startup-accelerator"
+                            )
+                        }
+                    />
+                </div>
+                <div className={"col-4"}>
+                    <img
+                        src={liaa}
+                        className={"partner_logo"}
+                        alt={"liaa"}
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                            window.open(
+                                "http://www.liaa.gov.lv/en/invest-latvia/start-up-ecosystem"
+                            )
+                        }
+                    />
+                </div>
+            </div>
+            <div style={{ padding: "25px 0px" }}></div>
         </div>
-        <div className={"col-3"}>
-          <img
-            src={skydeck}
-            className={"partner_logo"}
-            alt={"skydeck"}
-            style={{ cursor: "pointer" }}
-            onClick={() => window.open("https://skydeck.berkeley.edu/")}
-          />
-        </div>
-        <div className={"col-3"}>
-          <img
-            src={brinc}
-            className={"partner_logo"}
-            alt={"brinc"}
-            style={{ cursor: "pointer" }}
-            onClick={() => window.open("https://www.brinc.io/")}
-          />
-        </div>
-        <div className={"col-3"}>
-          <img
-            src={buildit}
-            className={"partner_logo"}
-            alt={"buildit"}
-            style={{ cursor: "pointer" }}
-            onClick={() => window.open("https://www.buildit.lv/")}
-          />
-        </div>
-      </div>
-      <div style={{ padding: "25px 0px" }}></div>
-      <div className={"row"}>
-        <div className={"col-3"}>
-          <img
-            src={hatch}
-            className={"partner_logo"}
-            alt={"digital_ocean"}
-            style={{ cursor: "pointer" }}
-            onClick={() => window.open("https://www.digitalocean.com/hatch/")}
-          />
-        </div>
-        <div className={"col-3"}>
-          <img
-            src={mongodb}
-            className={"partner_logo"}
-            alt={"mongodb"}
-            style={{ cursor: "pointer" }}
-            onClick={() =>
-              window.open("https://www.mongodb.com/startup-accelerator")
-            }
-          />
-        </div>
-        <div className={"col-3"}>
-          <img
-            src={liaa}
-            className={"partner_logo"}
-            alt={"liaa"}
-            style={{ cursor: "pointer" }}
-            onClick={() =>
-              window.open(
-                "http://www.liaa.gov.lv/en/invest-latvia/start-up-ecosystem"
-              )
-            }
-          />
-        </div>
-        <div className={"col-3"}>
-          <img
-            src={artesian}
-            className={"partner_logo"}
-            alt={"artesian"}
-            // style={{ cursor: "pointer" }}
-          />
-        </div>
-      </div>
-      <div style={{ padding: "25px 0px" }}></div>
     </div>
-  </div>
 )
 
 export default Partners
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,143 +16,152 @@ import Partners from "../components/partners"
 // import AnimatedText from "../components/textAnimated"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Spend, Send & Save Bitcoin on the Lightning Network" />
+    <Layout>
+        <SEO title="Spend, Send & Save Bitcoin on the Lightning Network" />
 
-    <div className={"page-header home"}>
-      <div
-        className={"container call-to-action row landing"}
-        style={{ background: "#f6f9fc", textAlign: "center" }}
-      >
-        <h1>
-          Spend, Send & Save <span style={{ color: "#ff9900" }}>Bitcoin</span>
-        </h1>
-        <p
-          className={"landing_subtitle"}
-          style={{ marginBottom: 40, fontWeight: 600, fontSize: 32 }}
+        <div className={"page-header home"}>
+            <div
+                className={"container call-to-action row landing"}
+                style={{ background: "#f6f9fc", textAlign: "center" }}
+            >
+                <h1>
+                    Spend, Send & Save{" "}
+                    <span style={{ color: "#ff9900" }}>Bitcoin</span>
+                </h1>
+                <p
+                    className={"landing_subtitle"}
+                    style={{ marginBottom: 40, fontWeight: 600, fontSize: 32 }}
+                >
+                    Your personal lightning powered Bitcoin debit card
+                </p>
+                <Subscribe />
+                {/* <AnimatedText /> */}
+            </div>
+            <img src={landing} alt="screens" className={"landing_img"} />
+        </div>
+        <section className={"sky"}>
+            <div
+                className={"container"}
+                style={{ marginBottom: 50, marginTop: 50 }}
+            >
+                <h2 className={"section_title"}>
+                    Towards a closed loop Bitcoin economy
+                </h2>
+                <img
+                    src={zeus}
+                    alt={"Store, Send, Spend Bitcoin Instantly"}
+                    className={"zeus"}
+                />
+                <div className="x1">
+                    <div className="cloud"></div>
+                </div>
+
+                <div className="x2">
+                    <div className="cloud"></div>
+                </div>
+
+                <div className="x3">
+                    <div className="cloud"></div>
+                </div>
+
+                <div className="x4">
+                    <div className="cloud"></div>
+                </div>
+
+                <div className="x5">
+                    <div className="cloud"></div>
+                </div>
+                <div className={"row"}>
+                    <div className={"col-4"}>
+                        <IconCard
+                            src={mobilepay}
+                            title={"Easy On-Ramps"}
+                            desc={
+                                "Get started with Bitcoin on the Lightning Network by simply purchasing Bitcoin with your credit card"
+                            }
+                        />
+                    </div>
+                    <div className={"col-4"}>
+                        <IconCard
+                            src={lightningbolt}
+                            title={"Easy Off-Ramps"}
+                            desc={
+                                "Send Bitcoin to anyone over social media readily available to use on the Lightning Network or spendable on a personal debit card"
+                            }
+                        />
+                    </div>
+                    <div className={"col-4"}>
+                        <IconCard
+                            src={easy}
+                            title={"Incentivized Spending"}
+                            desc={
+                                "Your personal IBAN account to spend fiat and earn Bitcoin cashbacks that you can spend & send instantly!"
+                            }
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section className={"features__section"}>
+            <div className={"container"}>
+                <h2 className={"section_title"}>
+                    Lastbit mobile (iOS & Android)
+                </h2>
+                <Features />
+            </div>
+        </section>
+
+        <div className={"showcase"}>
+            <h4 className={"showcase_title"}>
+                Try our testnet Bitcoin & Lightning wallet
+            </h4>
+            <p className={"showcase_desc"}>
+                Signup below to join the waitlist to use our completely new
+                application featuring virtual debit cards
+            </p>
+            <div className={"row container"} style={{ paddingBottom: 60 }}>
+                <img
+                    src={google}
+                    alt={"playstore"}
+                    className={"img-responsive hover-shadow"}
+                    style={{ maxWidth: "300px", margin: 20 }}
+                />
+                <img
+                    src={apple}
+                    alt={"appstore"}
+                    className={"img-responsive hover-shadow"}
+                    style={{ maxWidth: "300px", margin: 20 }}
+                />
+            </div>
+        </div>
+
+        <div
+            id="subscribe"
+            className={"call-to-action"}
+            style={{ padding: "100px 0px" }}
         >
-          Your personal lightning powered Bitcoin debit card
-        </p>
-        <Subscribe />
-        {/* <AnimatedText /> */}
-      </div>
-      <img src={landing} alt="screens" className={"landing_img"} />
-    </div>
-    <section className={"sky"}>
-      <div className={"container"} style={{ marginBottom: 50, marginTop: 50 }}>
-        <h2 className={"section_title"}>
-          Towards a closed loop Bitcoin economy
-        </h2>
-        <img
-          src={zeus}
-          alt={"Store, Send, Spend Bitcoin Instantly"}
-          className={"zeus"}
-        />
-        <div class="x1">
-          <div class="cloud"></div>
+            <div>
+                <div className={"call-to-action__content"}>
+                    <h2>
+                        The world's first Lightning powered consumer payments
+                        product
+                    </h2>
+                    <p>
+                        {" "}
+                        Join the waitlist to be the first to get personal
+                        physical cards & IBAN accounts to store, spend & earn
+                        Bitcoin over the Lightning Network
+                    </p>
+                </div>
+            </div>
+            <div className={"container"}>
+                <Subscribe />
+            </div>
         </div>
 
-        <div class="x2">
-          <div class="cloud"></div>
-        </div>
-
-        <div class="x3">
-          <div class="cloud"></div>
-        </div>
-
-        <div class="x4">
-          <div class="cloud"></div>
-        </div>
-
-        <div class="x5">
-          <div class="cloud"></div>
-        </div>
-        <div className={"row"}>
-          <div className={"col-4"}>
-            <IconCard
-              src={mobilepay}
-              title={"Easy On-Ramps"}
-              desc={
-                "Get started with Bitcoin on the Lightning Network by simply purchasing Bitcoin with your credit card"
-              }
-            />
-          </div>
-          <div className={"col-4"}>
-            <IconCard
-              src={lightningbolt}
-              title={"Easy Off-Ramps"}
-              desc={
-                "Send Bitcoin to anyone over social media readily available to use on the Lightning Network or spendable on a personal debit card"
-              }
-            />
-          </div>
-          <div className={"col-4"}>
-            <IconCard
-              src={easy}
-              title={"Incentivized Spending"}
-              desc={
-                "Your personal IBAN account to spend fiat and earn Bitcoin cashbacks that you can spend & send instantly!"
-              }
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section className={"features__section"}>
-      <div className={"container"}>
-        <h2 className={"section_title"}>Lastbit mobile (iOS & Android)</h2>
-        <Features />
-      </div>
-    </section>
-
-    <div className={"showcase"}>
-      <h4 className={"showcase_title"}>
-        Try our testnet Bitcoin & Lightning wallet
-      </h4>
-      <p className={"showcase_desc"}>
-        Signup below to join the waitlist to use our completely new application
-        featuring virtual debit cards
-      </p>
-      <div className={"row container"} style={{ paddingBottom: 60 }}>
-        <img
-          src={google}
-          alt={"playstore"}
-          className={"img-responsive hover-shadow"}
-          style={{ maxWidth: "300px", margin: 20 }}
-        />
-        <img
-          src={apple}
-          alt={"appstore"}
-          className={"img-responsive hover-shadow"}
-          style={{ maxWidth: "300px", margin: 20 }}
-        />
-      </div>
-    </div>
-
-    <div
-      id="subscribe"
-      className={"call-to-action"}
-      style={{ padding: "100px 0px" }}
-    >
-      <div>
-        <div className={"call-to-action__content"}>
-          <h2>The world's first Lightning powered consumer payments product</h2>
-          <p>
-            {" "}
-            Join the waitlist to be the first to get personal physical cards &
-            IBAN accounts to store, spend & earn Bitcoin over the Lightning
-            Network
-          </p>
-        </div>
-      </div>
-      <div className={"container"}>
-        <Subscribe />
-      </div>
-    </div>
-
-    <Partners />
-  </Layout>
+        <Partners />
+    </Layout>
 )
 
 export default IndexPage
