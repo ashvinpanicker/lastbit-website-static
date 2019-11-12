58a508c783f62ddfb359b2704195853bf6253562
agnjkafgh <agnjkafgh@gmail.com>
2019-11-11 18:18:39 -0800

    add century gothic font

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,68 +1,68 @@
 module.exports = {
-  siteMetadata: {
-    title: `lastbit`,
-    description: `Store and Spend Bitcoin, Securely and Instantly`,
-    author: `@ashvinpanicker`,
-    siteUrl: `https://www.lastbit.io`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        exclude: ["/terms", `/privacy`, `/contact`],
-      },
+    siteMetadata: {
+        title: `lastbit`,
+        description: `Store and Spend Bitcoin, Securely and Instantly`,
+        author: `@ashvinpanicker`,
+        siteUrl: `https://www.lastbit.io`,
     },
-    `gatsby-plugin-robots-txt`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/static/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#00204a`,
-        display: `minimal-ui`,
-        icon: `${__dirname}/static/images/logo.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-sass`,
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        google: {
-          families: ["PT Serif, OpenSans"],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-sitemap`,
+            options: {
+                exclude: ["/terms", `/privacy`, `/contact`],
+            },
         },
-        custom: {
-          families: ["Inter"],
-          urls: ["/fonts/fonts.css"],
+        `gatsby-plugin-robots-txt`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/static/images`,
+            },
         },
-      },
-    },
-    {
-      resolve: "gatsby-plugin-mailchimp",
-      options: {
-        endpoint:
-          "https://lastbit.us18.list-manage.com/subscribe/post?u=7063ad67b1481fb79537c8391&amp;id=ec0b596da5",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-124343236-2",
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#00204a`,
+                display: `minimal-ui`,
+                icon: `${__dirname}/static/images/logo.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-sass`,
+        {
+            resolve: "gatsby-plugin-web-font-loader",
+            options: {
+                google: {
+                    families: ["PT Serif, OpenSans"],
+                },
+                custom: {
+                    families: ["Century Gothic"],
+                    urls: ["/fonts/fonts.css"],
+                },
+            },
+        },
+        {
+            resolve: "gatsby-plugin-mailchimp",
+            options: {
+                endpoint:
+                    "https://lastbit.us18.list-manage.com/subscribe/post?u=7063ad67b1481fb79537c8391&amp;id=ec0b596da5",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-124343236-2",
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
