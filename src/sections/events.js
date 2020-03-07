8dada97f34e1a1e3dd6adb6378cef066e4096127
Ashvin Panicker <ashvin.panicker@gmail.com>
2020-03-07 06:23:36 +0530

    Add bitcoin 2020

--- src/sections/events.js
+++ src/sections/events.js
@@ -4,10 +4,38 @@ import boldawards from "../../static/images/events/boldawards.svg"
 import innoenergy from "../../static/images/events/innoenergy.svg"
 import techcrunch from "../../static/images/events/techcrunch.png"
 import money2020 from "../../static/images/events/money2020.png"
+import bitcoin2020 from "../../static/images/events/bitcoin2020.jpeg"
+import twitter from "../../static/images/icons/twitter.png"
 
 const Events = () => (
     <div className={"container"} style={{ textAlign: "center", marginTop: 50 }}>
         <h2>Events</h2>
+        <div className={"row"}>
+            <div className={"col-12"}>
+                <div className="card" style={{borderRadius: 20}}>
+                    <img
+                        src={bitcoin2020}
+                        className={"event_logo"}
+                        alt={"bitcoin2020"}
+                        style={{
+                            width: '80%'
+                        }}
+                    />
+                    <p className="desc" style={{marginTop: 20, fontWeight: 'bold'}}>
+                        Proudly Supporting {'  '}
+                        <a href="https://twitter.com/bitcoin2020conf" style={{textDecoration: 'underline'}}>
+                            <img
+                                src={twitter}
+                                alt={"twitter_icon"}
+                                style={{
+                                    width: 20, height: 20, verticalAlign:"middle"
+                                }}/>
+                            <span>@bitcoin2020conf</span>
+                        </a>, San Fransisco!
+                    </p>
+                </div>
+            </div>
+        </div>
         <div className={"row"}>
             <div className={"col-6"}>
                 <div className="card">
